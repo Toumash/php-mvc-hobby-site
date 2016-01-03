02f2ce36bdf23b442d70887b358f79c7839782a3
Tomasz Dłuski <mr.toumash@gmail.com>
2016-01-03 19:55:49 +0100

    logout button

--- templates/user/profile.php
+++ templates/user/profile.php
@@ -1,8 +1,12 @@
 <?php
 $user = $this->get('user');
 ?>
-<span>Login: <?php echo $user->login ?></span>
+<span>Login: <?php echo $user->login ?></span><br/>
 <span>Email: <?php echo $user->email ?></span>
 
 <br/>
 <strong><a href="<?php echo $this->generateUrl('user', 'photos'); ?>">Moje zdjęcia</a></strong>
+
+<br/><br/>
+
+<a href="<?php echo $this->generateUrl('authorization','logout'); ?>">WYLOGUJ</a>
